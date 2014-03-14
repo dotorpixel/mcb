597f26865ae04453228dd711e5a9c731530536b7
Agung Hari Wijaya <a9un9_ch@yahoo.co.id>
2014-03-14 23:52:00 +0700

    fix navigation style and font

--- home.php
+++ home.php
@@ -11,24 +11,23 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-	<nav class="navbar navbar-default" role="navigation">
-	  <div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	    </div>
+				<nav class="navbar" role="navigation">
+				    <!-- Brand and toggle get grouped for better mobile display -->
+				    <div class="navbar-header">
+				      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				    </div>
 
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	    	<?php wp_nav_menu( array( 'theme_location' => 'header','menu_class' => 'nav navbar-nav' ) ); ?>
-	    	<?php wp_nav_menu( array( 'theme_location' => 'sosmed','menu_class' => 'nav navbar-nav navbar-right' ) ); ?>
-	  </div><!-- /.container-fluid -->
-	</nav>
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				    <div class="row collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				    	<?php wp_nav_menu( array( 'theme_location' => 'header','menu_class' => 'nav navbar-nav' ) ); ?>
+				    	<?php wp_nav_menu( array( 'theme_location' => 'sosmed','menu_class' => 'nav navbar-nav navbar-right' ) ); ?>
+				  	
+				</nav>
 			</div>
 		</div>
 	</div>
